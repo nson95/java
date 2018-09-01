3b6ba5e39fc75b1a7f526e16102f2322b843f03b
nson95 <nicksonnenberg95@gmail.com>
2018-09-01 14:38:55 -0400

    made a hello world for java!

--- HelloWorld/src/com/example/helloworld/helloworld.java
+++ HelloWorld/src/com/example/helloworld/helloworld.java
@@ -1,4 +1,7 @@
 package com.example.helloworld;
 
 public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");  // this will print the string Hello World!!
+    }
 }
