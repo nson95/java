1d326868b16172f1841e04954a6e82eb7d177a09
nson95 <nicksonnenberg95@gmail.com>
2018-09-03 12:06:31 -0400

    First java app

--- HelloWorld/src/com/example/helloworld/helloworld.java
+++ HelloWorld/src/com/example/helloworld/helloworld.java
@@ -1,7 +1,7 @@
 package com.example.helloworld;
 
 public class helloworld {
-    public static void main(String[] args) {
+    public static void main(String[] rgs) {
         System.out.println("Hello World");  // this will print the string Hello World
     }
 
