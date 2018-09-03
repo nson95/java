65358fc0d75bdaffd9839e7411870c60f2416cda
nson95 <nicksonnenberg95@gmail.com>
2018-09-02 20:51:08 -0400

    playing around with HelloWorld

--- HelloWorld/src/com/example/helloworld/helloworld.java
+++ HelloWorld/src/com/example/helloworld/helloworld.java
@@ -2,6 +2,7 @@ package com.example.helloworld;
 
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");  // this will print the string Hello World!!
+        System.out.println("Hello World");  // this will print the string Hello World
     }
+
 }
