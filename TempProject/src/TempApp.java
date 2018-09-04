5ed480b5bdd9c27aa6baa89773fddebf5f007e51
nson95 <nicksonnenberg95@gmail.com>
2018-09-04 14:27:56 -0400

    messing around in eclipse

--- TempProject/src/TempApp.java
+++ TempProject/src/TempApp.java
@@ -1,7 +1,9 @@
 public class TempApp {
     public static void main(String[] args) {
     	System.out.println("This application is only temporary...");
-// runs class out on system and tells the class to print the line
+    	System.out.println("Success!!!!!!!!!!!!!");
+    	System.out.println("heyyyyyyyyy");
+// runs class 'out' on system and tells the class to print the line
 	}
 
 }
