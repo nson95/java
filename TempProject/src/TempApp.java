5b37bda21f6ca249a6f94d713033a2eb93ac1c9b
nson95 <nicksonnenberg95@gmail.com>
2018-09-04 15:49:58 -0400

    update for TempApp

--- TempProject/src/TempApp.java
+++ TempProject/src/TempApp.java
@@ -2,7 +2,7 @@ public class TempApp {
     public static void main(String[] args) {
     	System.out.println("This application is only temporary...");
     	System.out.println("Success!!!!!!!!!!!!!");
-    	System.out.println("heyyyyyyyyy");
+    	System.out.println("heyyyyyyyyy.");
     	System.out.println("Why hello again, I would like another commit please.");
 // runs class 'out' on system and tells the class 'out' to print the line
 	}
