76cb49d025f5fd1c520b9c5522387bfd8c7cfbf6
nson95 <nicksonnenberg95@gmail.com>
2018-09-04 15:17:25 -0400

    wroking through class 9/4

--- TempProject/src/TempApp.java
+++ TempProject/src/TempApp.java
@@ -3,7 +3,7 @@ public class TempApp {
     	System.out.println("This application is only temporary...");
     	System.out.println("Success!!!!!!!!!!!!!");
     	System.out.println("heyyyyyyyyy");
-// runs class 'out' on system and tells the class to print the line
+// runs class 'out' on system and tells the class 'out' to print the line
 	}
 
 }
