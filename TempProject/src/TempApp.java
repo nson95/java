63d171bb801db00c868f369595e44f412a27f5be
nson95 <nicksonnenberg95@gmail.com>
2018-09-04 15:33:32 -0400

    classwork 9.4

--- TempProject/src/TempApp.java
+++ TempProject/src/TempApp.java
@@ -3,6 +3,7 @@ public class TempApp {
     	System.out.println("This application is only temporary...");
     	System.out.println("Success!!!!!!!!!!!!!");
     	System.out.println("heyyyyyyyyy");
+    	System.out.println("Why hello again, I would like another commit please.");
 // runs class 'out' on system and tells the class 'out' to print the line
 	}
 
