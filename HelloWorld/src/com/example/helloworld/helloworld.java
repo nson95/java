bb2125bb5a6272fefd34fe7deca1e4d65853a93d
nson95 <nicksonnenberg95@gmail.com>
2018-09-04 11:58:46 -0400

    working during class

--- HelloWorld/src/com/example/helloworld/helloworld.java
+++ HelloWorld/src/com/example/helloworld/helloworld.java
@@ -2,7 +2,8 @@ package com.example.helloworld;
 
 public class helloworld {
     public static void main(String[] rgs) {
-        System.out.println("Hello World");  // this will print the string Hello World
+        System.out.println("Hello World");
+        System.out.println("Why hello again.."); // this will print the string Hello World
     }
 
 }
