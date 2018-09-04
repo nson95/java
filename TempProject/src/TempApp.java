5ba8f2d416f0ed34725ec5568a79a7ad6386b26b
nson95 <nicksonnenberg95@gmail.com>
2018-09-04 14:05:27 -0400

    more messin in eclipse

--- TempProject/src/TempApp.java
+++ TempProject/src/TempApp.java
@@ -1,6 +1,6 @@
 public class TempApp {
     public static void main(String[] args) {
-    	System.out.println("Hello World!");
+    	System.out.println("This application is only temporary");
 
 	}
 
