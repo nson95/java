de3a2dfb8d5ab3c21d99c2054603b1ec6e83102c
nson95 <nicksonnenberg95@gmail.com>
2018-09-04 14:11:22 -0400

    more play no work

--- TempProject/src/TempApp.java
+++ TempProject/src/TempApp.java
@@ -1,7 +1,7 @@
 public class TempApp {
     public static void main(String[] args) {
-    	System.out.println("This application is only temporary");
-
+    	System.out.println("This application is only temporary...");
+// runs class out on system and tells the class to print the line
 	}
 
 }
