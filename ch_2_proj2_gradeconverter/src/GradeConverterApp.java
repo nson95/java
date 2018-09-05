208fc5f8bd4516bbeffb57945af3ed55298d5294
nson95 <nicksonnenberg95@gmail.com>
2018-09-05 15:39:42 -0400

    example for project ch 2

---
+++ ch_2_proj2_gradeconverter/src/GradeConverterApp.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class GradeConverterApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Letter Grade Converter.");
+		Scanner sc = new Scanner(System.in);
+		
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+		System.out.print("Enter Numerical Grade:  ");
+		int numericalGrade = sc.nextInt();
+		
+		String letterGrade = "";
+		
+		if (numericalGrade >= 88) {
+			letterGrade = "A";
+		} else if (numericalGrade >= 80) {
+			letterGrade = "B";
+		} else if (numericalGrade >= 68) {
+			letterGrade = "C";
+		} else if (numericalGrade >= 60) {
+			letterGrade = "D";
+		} else letterGrade = "F";
+		
+			
+			
+		
+		System.out.println("Letter grade: "+letterGrade);
+		
+		System.out.println("Continue? (y/n)");
+		choice = sc.next();
+		
+	}
+		sc.close();
+		System.out.println("Goodbye!");
+
+	}
+
+}
