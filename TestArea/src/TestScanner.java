2fd000af2bef19ec57c2b8a6814f3d258413eed4
nson95 <nicksonnenberg95@gmail.com>
2018-09-05 10:02:01 -0400

    Trying to make a scanner that works.

---
+++ TestArea/src/TestScanner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TestScanner {
+
+	public static void main(String[] args) {
+		System.out.println("Please enter your age:    ");
+		System.out.println();
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter your age:    ");
+		int age = sc.int();
+
+	}
+
+}
