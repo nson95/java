2b273a43945cc5aaa790aa4c3f7c5419d36edc8f
nson95 <nicksonnenberg95@gmail.com>
2018-09-05 10:12:26 -0400

    Got input field working in scanner!

--- TestArea/src/TestScanner.java
+++ TestArea/src/TestScanner.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class TestScanner {
 
 	public static void main(String[] args) {
-		System.out.println("Please enter your age:    ");
+		System.out.println("Please enter your age:  ");
 		System.out.println();
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Please enter your age:    ");
-		int age = sc.int();
+		System.out.println("Please enter your age:  ");
+		int age = sc.nextInt();
 
 	}
 
 }
+
