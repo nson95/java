b26bbc83f45c6033268e5fa4c01b615753939837
nson95 <nicksonnenberg95@gmail.com>
2018-09-05 20:50:15 -0400

    rectangle calc app loop is giving me fits

---
+++ ch2_proj3_rectanglecalc/src/RectangleCalcApp.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class RectangleCalcApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Area and Perimeter Calculator");
+		System.out.println();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+		
+		System.out.println("Enter length: ");
+		double len = sc.nextDouble();
+		System.out.println("Enter width: ");
+		double wid = sc.nextDouble();
+		
+		double area = len * wid;
+		double per = 2 * len + 2 * wid;
+		System.out.println("Area: " +area);
+		System.out.println("Perimeter: " +per);
+		System.out.println();
+		
+	
+		System.out.println("continue (y/n)");
+		String choice = sc.next();
+		
+		} if (!choice.equalsIgnoreCase("y")) {
+			System.out.println("Hope you had a great time, goodbye.");
+		
+		}
+		sc.close();
+
+	}
+
+}
