a2c97dd66a4536ad86a285969e937b0da59f7a7c
nson95 <nicksonnenberg95@gmail.com>
2018-09-05 20:03:59 -0400

    done with proj 2-1

--- ch2_proj1_studentregistration/src/StudentRegApp.java
+++ ch2_proj1_studentregistration/src/StudentRegApp.java
@@ -9,11 +9,18 @@ public class StudentRegApp {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter first name: ");
 		String firstName = sc.next();
-		System.out.println();
-		System.out.println("Enter last name: ");
+		System.out.print("Enter last name: ");
 		String lastName = sc.next();
 		System.out.print("Enter year of birth: ");
 		int birthYear = sc.nextInt();
+		
+		System.out.println();
+		
+		String message = "Welcome " +firstName +" " +lastName +"!";
+		String pswd = firstName +"*" +birthYear;
+		System.out.println(message);
+		System.out.println("Your registration is complete.");
+		System.out.println("Your temporary password is: " +pswd);
 		
 		
 
