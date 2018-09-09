dbdfbc991df3785ec25ddbf93ccdca288ed6e524
nson95 <nicksonnenberg95@gmail.com>
2018-09-09 18:35:06 -0400

    working dice roller app

--- ch5_proj1_dicerollerapp/src/DiceRollerApp.java
+++ ch5_proj1_dicerollerapp/src/DiceRollerApp.java
@@ -5,25 +5,36 @@ public class DiceRollerApp {
 	public static void main(String[] args) {
 		System.out.println("Dice Roller");
 		System.out.println();
+		int a=0; 
+		int b=0;
 		Scanner sc = new Scanner(System.in);
-		
 		System.out.print("Roll the dice? (y/n): ");
 		String choice = sc.next();
-		System.out.println();
-		int a = 0;
-		int b=0;
 		while (choice.equalsIgnoreCase("y")) {
 			a = (int) (Math.random() * 6 + 1);
 			b = (int) (Math.random() * 6 + 1);
-		System.out.print("Die 1: " +a +"\n");
-		System.out.print("Die 2: " +b +"\n");
-		System.out.print("Roll Again? (y/n): ");
-		choice = sc.next();
-		}
+			if (a == 1 && b == 1) {
+				System.out.print("Die 1: " +a +"\n");
+				System.out.print("Die 2: " +b +"\n");
+				System.out.println("Snake eyes!");
+			} else if (a==6 && b==6) {
+				System.out.print("Die 1: " +a +"\n");
+				System.out.print("Die 2: " +b +"\n");
+				System.out.println("Boxcars!");
+				
+			} else {
+				System.out.print("Die 1: " +a +"\n");
+				System.out.print("Die 2: " +b +"\n");
+				}
+			System.out.print("Roll Again? (y/n): " +"\n");
+			choice = sc.next();
+		} sc.close();
 		
-
-		
-		System.out.println("goodbye");
+	    
 	}
-
+	
+		
+		
 }
+
+
