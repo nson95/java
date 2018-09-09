8fe4cdbec08a0031209ec4ab6b5f664c5a206663
nson95 <nicksonnenberg95@gmail.com>
2018-09-09 12:28:35 -0400

    everything but the snake eyes!

--- ch5_proj1_dicerollerapp/src/DiceRollerApp.java
+++ ch5_proj1_dicerollerapp/src/DiceRollerApp.java
@@ -1,18 +1,27 @@
+import java.util.Scanner;
 
 public class DiceRollerApp {
 
 	public static void main(String[] args) {
 		System.out.println("Dice Roller");
 		System.out.println();
+		Scanner sc = new Scanner(System.in);
 		
-		String choice = "y";
+		System.out.print("Roll the dice? (y/n): ");
+		String choice = sc.next();
+		System.out.println();
+		int a = 0;
+		int b=0;
 		while (choice.equalsIgnoreCase("y")) {
-			
+			a = (int) (Math.random() * 6 + 1);
+			b = (int) (Math.random() * 6 + 1);
+		System.out.print("Die 1: " +a +"\n");
+		System.out.print("Die 2: " +b +"\n");
+		System.out.print("Roll Again? (y/n): ");
+		choice = sc.next();
 		}
+		
 
-		
-		
-		
 		
 		System.out.println("goodbye");
 	}
