9426ccaa1ea5303aa3b3e4ef973af123b71e574a
nson95 <nicksonnenberg95@gmail.com>
2018-09-14 12:28:13 -0400

    ArrayList demo

---
+++ TestArea/src/ArrayListDemoApp.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class ArrayListDemoApp {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> numbers = new ArrayList<>();
+		
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		
+		
+		System.out.println(numbers);
+
+	}
+
+}
