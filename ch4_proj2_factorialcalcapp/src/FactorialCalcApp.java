02fa7d6482d201243d2de565042b6bdf2f30c883
nson95 <nicksonnenberg95@gmail.com>
2018-09-16 06:58:30 -0400

    halfway done with ch4 proj2

---
+++ ch4_proj2_factorialcalcapp/src/FactorialCalcApp.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FactorialCalcApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Factorial Calculator");
+		System.out.println();
+		Scanner sc = new Scanner(System.in);
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+			System.out.println("Enter an integer that's greater than 0 and less than 10: ");
+			int n = sc.nextInt();
+			String numbers = "";
+			for ( 
+			
+		}
+
+	}
+
+}
