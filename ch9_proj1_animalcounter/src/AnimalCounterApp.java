3a7c2e7fbdbf002fffce645638710e0e5b68085a
nson95 <nicksonnenberg95@gmail.com>
2018-09-16 14:58:27 -0400

    Finished ch9 proj1 with try catch instead of throws exception

--- ch9_proj1_animalcounter/src/AnimalCounterApp.java
+++ ch9_proj1_animalcounter/src/AnimalCounterApp.java
@@ -1,11 +1,16 @@
 
 public class AnimalCounterApp {
 
-	public static void main(String[] args) throws CloneNotSupportedException {
+	public static void main(String[] args) {
 		System.out.println("Counting alligators...");
 		Alligator a = new Alligator();
 		Sheep s = new Sheep("Blackie");
-		Sheep s2 = (Sheep) s.clone();
+		Sheep s2 = null;
+		try {
+		s2 = (Sheep) s.clone(); 
+		} catch (CloneNotSupportedException e) {
+			System.out.println("Clone not available");
+		}
 		s2.setName("Dolly");
 		count(a, 3);
 		System.out.println();
@@ -16,6 +21,7 @@ public class AnimalCounterApp {
 		System.out.println();
 		count(s, 1);
 		System.out.println("Goodbye!");
+	
 
 	}
 	public static void count(Countable c, int maxcount) {
