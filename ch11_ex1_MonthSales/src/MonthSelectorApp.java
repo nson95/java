f776ccfa0ceef862c5205ea5f23b2562597080b3
nson95 <nicksonnenberg95@gmail.com>
2018-09-19 15:20:11 -0400

    ch11 excersize 1

--- ch11_ex1_MonthSales/src/MonthSelectorApp.java
+++ ch11_ex1_MonthSales/src/MonthSelectorApp.java
@@ -9,34 +9,47 @@ public class MonthSelectorApp {
 
 
         // get currency formatting
-        NumberFormat currency = NumberFormat.getCurrencyInstance();
         
-        String months[] = new String[12];
-        months[0] = "January";
-        months[1] = "February";
-        months[2] = "March";
-        months[3] = "April";
-        months[4] = "May";
-        months[5] = "June";
-        months[6] = "July";
-        months[7] = "August";
-        months[8] = "September";
-        months[9] = "October";
-        months[10] = "November";
-        months[11] = "December";
         
-        double monthSales[] = {191.88, 72398.2, 72930.2, 63294.2, 382422.2, 247193.7, 
-        		46238.4, 93472.3, 3802938.4, 829381.7, 724934.75, 82461.24};
-        for (int i = 0; i < months.length; i++) {
-        	System.out.println(months[i] +" " +monthSales[i]);
-        }
+        
+        
         // get one or more months
         String choice = "y";
         while (choice.equalsIgnoreCase("y")) {
             // get the input from the user
-        	
+        	String months[] = new String[12];
+        	months[0] = "January";
+        	months[1] = "February";
+        	months[2] = "March";
+        	months[3] = "April";
+        	months[4] = "May";
+        	months[5] = "June";
+        	months[6] = "July";
+        	months[7] = "August";
+        	months[8] = "September";
+        	months[9] = "October";
+        	months[10] = "November";
+        	months[11] = "December";
+        	double monthSales[] = {191.88, 72398.2, 72930.2, 63294.2, 382422.2, 247193.7, 
+        			46238.4, 93472.3, 3802938.4, 829381.7, 724934.75, 82461.24};
         	int monthNumber = Console.getInt("Enter month number: ");
+        	for (int i = 0; i < months.length; i++) {
+        		
+        		NumberFormat currency = NumberFormat.getCurrencyInstance();
+        		
+        		
+        		
+        		System.out.println("Sales for " +months[monthNumber-1] +": " +currency.format(monthSales[monthNumber-1]));
+        		
+        		
+        	
+        		
+        		
+        		
             
+        		choice = Console.getString("Continue? (y/n): ");
+        		Console.displayLine();
+        	
             
             
             // validate input
@@ -50,8 +63,6 @@ public class MonthSelectorApp {
 
 
             // check if the user wants to continue
-            choice = Console.getString("Continue? (y/n): ");
-            Console.displayLine();
         }
         
         // display the total sales for the year
@@ -60,4 +71,6 @@ public class MonthSelectorApp {
         Console.displayLine();
     }    
 
+    }
+    
 }
