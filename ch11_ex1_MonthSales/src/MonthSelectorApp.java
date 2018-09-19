f8124ade0c7e0796d34872d30560e882984a5f99
nson95 <nicksonnenberg95@gmail.com>
2018-09-19 09:37:53 -0400

    ch 11 excersizes working with arrays

---
+++ ch11_ex1_MonthSales/src/MonthSelectorApp.java
@@ -0,0 +1,63 @@
+import java.text.NumberFormat;
+
+public class MonthSelectorApp {
+
+    public static void main(String[] args) {
+        System.out.println("Monthly Sales\n");
+        
+        // declare monthNames and monthSales arrays
+
+
+        // get currency formatting
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        
+        String months[] = new String[12];
+        months[0] = "January";
+        months[1] = "February";
+        months[2] = "March";
+        months[3] = "April";
+        months[4] = "May";
+        months[5] = "June";
+        months[6] = "July";
+        months[7] = "August";
+        months[8] = "September";
+        months[9] = "October";
+        months[10] = "November";
+        months[11] = "December";
+        
+        double monthSales[] = {191.88, 72398.2, 72930.2, 63294.2, 382422.2, 247193.7, 
+        		46238.4, 93472.3, 3802938.4, 829381.7, 724934.75, 82461.24};
+        for (int i = 0; i < months.length; i++) {
+        	System.out.println(months[i] +" " +monthSales[i]);
+        }
+        // get one or more months
+        String choice = "y";
+        while (choice.equalsIgnoreCase("y")) {
+            // get the input from the user
+        	
+        	int monthNumber = Console.getInt("Enter month number: ");
+            
+            
+            
+            // validate input
+         if (monthNumber < 1 || monthNumber > months.length) {
+                Console.displayLine("Invalid month number. Try again.");
+                continue;                
+            }
+            
+            // get the index number for the month
+            // and display the month name and sales            
+
+
+            // check if the user wants to continue
+            choice = Console.getString("Continue? (y/n): ");
+            Console.displayLine();
+        }
+        
+        // display the total sales for the year
+        
+        
+        Console.displayLine();
+    }    
+
+}
