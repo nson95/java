e7594c7ead5c46270f5e6f7ed9b3d3b423c4b9b8
nson95 <nicksonnenberg95@gmail.com>
2018-09-20 09:55:25 -0400

    proj11 work

---
+++ ch11_proj2_studentscores/src/ch11_proj2_studentscores/Student.java
@@ -0,0 +1,15 @@
+package ch11_proj2_studentscores;
+
+public class Student implements Comparable {
+	private int score;
+	private String firstName;
+	private String lastName;
+	
+
+	@Override
+	public int compareTo(Object arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
