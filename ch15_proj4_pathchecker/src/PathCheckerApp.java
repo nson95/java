f423aa03bc948fa275a3564a60afb37ea9971953
nson95 <nicksonnenberg95@gmail.com>
2018-09-24 20:24:16 -0400

    finished ch15 proj4

---
+++ ch15_proj4_pathchecker/src/PathCheckerApp.java
@@ -0,0 +1,36 @@
+import java.io.File;
+
+
+public class PathCheckerApp {
+
+	public static void main(String[] args) {
+		System.out.println("Path Checker");
+		System.out.println();
+		
+		String choice = "y";
+		
+		while (choice.equalsIgnoreCase("y")) {
+			String path = null;
+			path = Console.getString("Enter a path: ");
+			File file = new File(path);
+			if (file.exists()) {
+				if (file.isFile()) {
+					System.out.println("That path points to a file.");
+				}
+				else if (file.isDirectory()) {
+					System.out.println("That path points to a directory.");
+				}
+				
+				
+			} else {
+				System.out.println("That path does not exist.");
+			}
+			choice = Console.getString("Continue? (y/n): ");
+		}
+		
+		
+
+		System.out.println("Goodbye!");
+	}
+
+}
