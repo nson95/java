be1e3494cf107c144a0299362eb78e32c1b79fae
nson95 <nicksonnenberg95@gmail.com>
2018-11-19 23:24:07 -0500

    initial commit

---
+++ ch5proj2DataValidation/src/ch5proj2DataValidation/DataValidationApp.java
@@ -0,0 +1,26 @@
+package ch5proj2DataValidation;
+
+import java.util.Scanner;
+
+public class DataValidationApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Area and Perimeter Calculator");
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
+		double wid = getDoubleWithinRange(sc, "Enter width: ", 0, 60000);
+		double len = getDoubleWithinRange(sc, "Enter length: ", 0, 60000);
+		System.out.print("Area: " +(wid*len) +"\n"
+				+"Perimeter: " +(wid*2)+(len*2));
+		
+	}
+	public static double getDoubleWithinRange(Scanner sc, String prompt, double min, double max ) {
+		System.out.print(prompt);
+		double num = sc.nextDouble();
+		if (num<min||num>max) {
+			System.out.println("Error number must be between "+min +" and " +max);
+			System.out.print(prompt);
+		}
+		return num;
+	}
+}
