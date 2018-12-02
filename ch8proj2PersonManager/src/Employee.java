f92e9dca91fef3649cf08dcc1a5630d064b139a3
nson95 <nicksonnenberg95@gmail.com>
2018-12-02 10:45:36 -0500

    person, customer and employee

--- ch8proj2PersonManager/src/Employee.java
+++ ch8proj2PersonManager/src/Employee.java
@@ -1,5 +1,24 @@
 
 
 public class Employee extends Person {
+	private String ssn;
+	
+	public Employee(String firstName, String lastName, String ssn) {
+		super(firstName, lastName);
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee: " +firstName +" " +lastName +"ssn: " + ssn + "]";
+	}
 	
 }
