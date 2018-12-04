2a54d92ea7d538d50db8d15611f4a9595d39df72
nson95 <nicksonnenberg95@gmail.com>
2018-12-04 14:46:16 -0500

    customers can enter names

--- ch8proj2PersonManager/src/PersonManagerApp.java
+++ ch8proj2PersonManager/src/PersonManagerApp.java
@@ -5,7 +5,11 @@ public class PersonManagerApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to the Person Manager App ");
-
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+			String nc = Console.getString("Create customer or employee? (c/e): ", "c", "e");
+			
+		}
 	}
 
 }
