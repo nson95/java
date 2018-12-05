38fb6e3567499d22de72a0ece16e9cdf98dc0c88
nson95 <nicksonnenberg95@gmail.com>
2018-12-05 16:46:57 -0500

    basic logic finished

--- ch8proj2PersonManager/src/PersonManagerApp.java
+++ ch8proj2PersonManager/src/PersonManagerApp.java
@@ -8,8 +8,22 @@ public class PersonManagerApp {
 		String choice = "y";
 		while (choice.equalsIgnoreCase("y")) {
 			String nc = Console.getString("Create customer or employee? (c/e): ", "c", "e");
-			
+			if (nc.equalsIgnoreCase("c")) {
+				String fn = Console.getString("Please enter your first name: ");
+				String ln = Console.getString("Please enter your last name: ");
+				Customer c = new Customer(fn, ln, "82");
+				
+			} 
+			if (nc.equalsIgnoreCase("e")) {
+				String fn = Console.getString("Please enter your first name: ");
+				String ln = Console.getString("Please enter your last name: ");
+				String ssn = Console.getString("Please enter your social security number: ");
+				Employee e = new Employee(fn, ln, ssn);
+				System.out.println("Employee successfully registered: " +fn +" " +ln);
+			}
+			choice = Console.getString("Continue?", "y", "n");
 		}
+		System.out.println("Goodbye!");
 	}
 
 }
