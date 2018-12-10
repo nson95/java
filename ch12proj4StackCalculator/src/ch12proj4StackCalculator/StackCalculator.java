17e0cb28120d889dae586cbc1e3eb3bb9e437685
nson95 <nicksonnenberg95@gmail.com>
2018-12-10 14:48:18 -0500

    add and subtract finished

---
+++ ch12proj4StackCalculator/src/ch12proj4StackCalculator/StackCalculator.java
@@ -0,0 +1,31 @@
+package ch12proj4StackCalculator;
+
+import java.util.LinkedList;
+
+public class StackCalculator {
+	LinkedList<Double> stack = new LinkedList<>();
+	public void push(double x) {
+	stack.add(0, x);
+	}
+	public void pop() {
+		stack.remove(0);
+	}
+	public void add() {
+		double a = stack.remove(stack.size());
+		double b = stack.remove(stack.size()-1);
+		double c = a+b;
+		stack.add(c);
+	}
+	public void subtract() {
+		double a = stack.remove(stack.size());
+		double b = stack.remove(stack.size()-1);
+		double c  = 0;
+		if (a>b) {
+			c = a-b;
+		}
+		else if (a<b) {
+			c = b-a;
+		}
+		stack.add(c);
+	}
+}
---
+++ ch12proj4StackCalculator/src/ch12proj4StackCalculator/StackCalculatorApp.java
@@ -0,0 +1,10 @@
+package ch12proj4StackCalculator;
+
+public class StackCalculatorApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
