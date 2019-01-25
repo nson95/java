6fa4e4065bb1c4d116e2f070ea63263c963688a9
nson95 <nicksonnenberg95@gmail.com>
2019-01-25 15:27:56 -0500

    completed project 2-3

--- ch2proj3RectangleAreaCalculator/src/ch2proj3RectangleAreaCalculator/RectangleCalcApp.java
+++ ch2proj3RectangleAreaCalculator/src/ch2proj3RectangleAreaCalculator/RectangleCalcApp.java
@@ -1,9 +1,26 @@
 package ch2proj3RectangleAreaCalculator;
 
+import java.util.Scanner;
+
 public class RectangleCalcApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome to the Area and Perimeter Calculator");
+		System.out.println();
+		String choice = "y";
+			while (choice.equalsIgnoreCase("y")) {
+				Scanner sc = new Scanner(System.in);
+				System.out.println("Enter length: ");
+				double len = sc.nextDouble();
+				System.out.println("Enter width: ");
+				double wid = sc.nextDouble();
+				System.out.println("Area: " +(len*wid) +"\r"
+						+"Perimeter: " +(len+len)+(wid+wid));
+				System.out.print("Continue? (y/n) ");
+				choice = sc.next();
+			}
+			System.out.println("goodbye!");
+		
 
 	}
 
