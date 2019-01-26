64679a32b45ac29beb79ef0e4cbf5fbe13f93e06
nson95 <nicksonnenberg95@gmail.com>
2019-01-26 14:09:12 -0500

    finished project 4-2

--- ch4proj2FactorialCalculator/src/ch4proj2FactorialCalculator/FactorialCalculatorApp.java
+++ ch4proj2FactorialCalculator/src/ch4proj2FactorialCalculator/FactorialCalculatorApp.java
@@ -1,10 +1,26 @@
 package ch4proj2FactorialCalculator;
 
+import java.util.Scanner;
+
 public class FactorialCalculatorApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to the Factorial Calculator");
+		System.out.println();
+		Scanner sc = new Scanner(System.in);
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+		System.out.println("Enter an integer that's greater than 0 and less than 10: ");
+		int n = sc.nextInt();
+		long ans = 1;
+		for (int i = 1; i<=n; i++) {
+			ans*=i;
+		}
+		System.out.println("The factorial of " +n +" is " +ans);
+		System.out.print("Continue? (y/n) ");
+		choice = sc.next();
+		}
+		System.out.println("Goodbye!");
 	}
 
 }
