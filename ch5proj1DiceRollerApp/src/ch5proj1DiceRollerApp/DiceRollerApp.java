a901db88956adba5e838c5bfaee9e23e141c01a9
nson95 <nicksonnenberg95@gmail.com>
2019-01-27 17:31:28 -0500

    made dice roll function

--- ch5proj1DiceRollerApp/src/ch5proj1DiceRollerApp/DiceRollerApp.java
+++ ch5proj1DiceRollerApp/src/ch5proj1DiceRollerApp/DiceRollerApp.java
@@ -1,10 +1,23 @@
 package ch5proj1DiceRollerApp;
 
+import java.util.Scanner;
+
 public class DiceRollerApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Dice Roller");
+		System.out.println();
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Roll the dice? (y/n): ");
+		String choice = sc.next();
+		while (choice.equalsIgnoreCase("y")) {
+			
+		}
+	}
+	
+	public static int rollTheDice() {
+		int ans = (int) ((Math.random() * 6) +1);
+		return ans;
 	}
 
 }
