d28207aeece9d16f5c21db0fa278a632855169a0
nson95 <nicksonnenberg95@gmail.com>
2019-01-29 12:59:46 -0500

    progress

--- ch5proj1DiceRollerApp/src/ch5proj1DiceRollerApp/DiceRollerApp.java
+++ ch5proj1DiceRollerApp/src/ch5proj1DiceRollerApp/DiceRollerApp.java
@@ -11,8 +11,15 @@ public class DiceRollerApp {
 		System.out.print("Roll the dice? (y/n): ");
 		String choice = sc.next();
 		while (choice.equalsIgnoreCase("y")) {
-			
+			int a = rollTheDice();
+			int b = rollTheDice();
+			System.out.println("Die 1: " +a +"\r"
+					+"Die 2: " +b);
+			System.out.print("Continue? (y/n) ");
+			choice = sc.next();
 		}
+		System.out.println("goodbye!");
+		sc.close();
 	}
 	
 	public static int rollTheDice() {
