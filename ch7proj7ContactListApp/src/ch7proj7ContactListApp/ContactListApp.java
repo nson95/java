b54e11187e618a7899f615745879cbb4446b27bf
nson95 <nicksonnenberg95@gmail.com>
2019-02-02 23:34:17 -0500

    initial commit

--- ch7proj7ContactListApp/src/ch7proj7ContactListApp/ContactListApp.java
+++ ch7proj7ContactListApp/src/ch7proj7ContactListApp/ContactListApp.java
@@ -3,7 +3,7 @@ package ch7proj7ContactListApp;
 public class ContactListApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome to the Contact List application");
 
 	}
 
