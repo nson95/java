f0bee1329208f87d3ca58f47736685e2020fa080
nson95 <nicksonnenberg95@gmail.com>
2019-06-10 13:11:42 -0400

    progress

--- ch4proj3TipCalculator/src/ch4proj3TipCalculator/TipCalcApp.java
+++ ch4proj3TipCalculator/src/ch4proj3TipCalculator/TipCalcApp.java
@@ -1,6 +1,7 @@
 package ch4proj3TipCalculator;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class TipCalcApp {
@@ -8,7 +9,21 @@ public class TipCalcApp {
 	public static void main(String[] args) {
 		System.out.println("Tip Calculator \n");
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Cost of meal: ");
-		BigDecimal cost = sc.nextBigDecimal();
+		String choice = "y";
+		while(choice.equalsIgnoreCase("y")) {
+			System.out.println("Cost of meal: ");
+			BigDecimal cost = sc.nextBigDecimal();
+				for(double tp = .15; tp <= .25; tp +=.05) {
+					BigDecimal perc = new BigDecimal(Double.toString(tp));
+					BigDecimal tip = cost.multiply(perc);
+					BigDecimal total = cost.add(tip);
+					
+					System.out.println(NumberFormat.getPercentInstance().format(perc));
+					System.out.println("Tip Amount: " + NumberFormat.getCurrencyInstance().format(tip));
+					System.out.println("Total: " +NumberFormat.getCurrencyInstance().format(total));
+				}
+		System.out.println("Continue? (y/n)");
+		choice = sc.next();
+		}
 	}
 }
